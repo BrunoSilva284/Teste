567f6ed5931543b79fcb065e969088b09215f9d0
Da Silva <bruno.a.da.silva@accenture.com>
2017-05-08 19:18:48 -0300

    Primeiro arquivo

---
+++ JavaUm.java
@@ -0,0 +1,10 @@
+/**
+ * Created by bruno.a.da.silva on 08/05/2017.
+ */
+public class JavaUm
+{
+    public static void main (String Args[])
+    {
+        System.out.println("Teste165656");
+    }
+}
